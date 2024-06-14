1fe9a538ea206f2a96a67b962219dc5634e0e2de
Augustoluissilva <luis.agsilva22@gmail.com>
2024-06-14 15:05:48 -0300

    telas concluidas em 14/06/2024

---
+++ Positive-Mind-Aplicativo/src/screens/AlertCadastro.js
@@ -0,0 +1,123 @@
+import React, { useRef } from 'react';
+import { View, Text, StyleSheet, Animated, TouchableOpacity, Easing, Image } from 'react-native';
+
+export default function App() {
+  const slideAnim = useRef(new Animated.Value(0)).current;
+  const fadeAnim = useRef(new Animated.Value(1)).current; 
+
+  const startAnimation = () => {
+    Animated.parallel([
+      Animated.timing(slideAnim, {
+        toValue: -600,
+        duration: 500,
+        useNativeDriver: true,
+        easing: Easing.ease,
+      }),
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
+
+  return (
+    <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
+      <Animated.View style={[styles.alertBox, { transform: [{ translateY: slideAnim }] }]}>
+        <View style={styles.header}>
+          <Image source={require('./assets/image 5 (3).png')} style={styles.logo} />
+          <Text style={styles.companyName}>Positive Mind</Text>
+        </View>
+        <Text style={styles.title}>Seu cadastro foi feito com sucesso!</Text>
+        <Image source={require('./assets/image-removebg-preview (16) 1 (1).png')} style={styles.image} />
+        <Text style={styles.subtitle}>Enviamos um e-mail de confirmação para</Text>
+        <Text style={styles.email}>teste@email.com</Text>
+      </Animated.View>
+      <Animated.View style={[styles.buttonContainer, { opacity: fadeAnim }]}>
+        <TouchableOpacity style={styles.button} onPress={startAnimation}>
+          <Text style={styles.arrow}>↑</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E7FFF3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  logo: {
+    width: 30,
+    height: 30,
+    resizeMode: 'contain',
+    marginRight: 10,
+  },
+  companyName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  alertBox: {
+    width: '90%',
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    resizeMode: 'contain',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  email: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 30,
+    right: 30,
+    opacity: 1, 
+  },
+  button: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#4CAF50',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  arrow: {
+    fontSize: 24,
+    color: '#FFFFFF',
+  },
+});
---
+++ Positive-Mind-Aplicativo/src/screens/RedefinirSenha.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+const ResetPasswordScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require('./assets/image 5 (2).png')} style={styles.logoImage} />
+        <Text style={styles.logoText}>Positive Mind</Text>
+      </View>
+      <Image source={require('./assets/image 7.png')} style={[styles.image, styles.centeredImage]} />
+      <View style={styles.formContainer}>
+        <Text style={styles.title}>Redefinir senha</Text>
+        <Text style={styles.subtitle}>
+          Digite seu email e enviaremos um email para você informando como recuperá-la.
+        </Text>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputWrapper}>
+            <Image source={require('./assets/mail.png')} style={styles.inputIcon} />
+            <TextInput 
+              style={styles.input} 
+              placeholder="exemplo@teste.com" 
+              placeholderTextColor="#9fa5aa"
+            />
+          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.loginLink}>
+          <Text style={styles.loginText}>Lembrou sua senha? Login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EAF4F4',
+    alignItems: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 50,
+    marginBottom: 20,
+  },
+  logoImage: {
+    width: 30, 
+    height: 30, 
+    marginRight: 10,
+  },
+  logoText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#3A6351',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  centeredImage: {
+    alignSelf: 'center', // Centraliza a imagem horizontalmente
+  },
+  formContainer: {
+    width: '90%',
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+    marginTop: 50, 
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#7E7E7E',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: '100%',
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    borderColor: '#E1E1E1',
+    borderWidth: 1,
+  },
+  inputIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    color: '#333',
+    paddingVertical: 10,
+  },
+  button: {
+    width: '100%',
+    backgroundColor: '#3A6351',
+    borderRadius: 8,
+    padding: 15,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+  loginLink: {
+    marginTop: 20,
+  },
+  loginText: {
+    fontSize: 14,
+    color: '#3A6351',
+  },
+});
+
+export default ResetPasswordScreen;
